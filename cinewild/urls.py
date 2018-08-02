74d901dcc48d68321118b8063a5cda4ed5e77b91
fredlnl <f.lang@hotmail.fr>
2018-08-02 10:31:34 +0200

    add dates for Actors and Movies

--- cinewild/urls.py
+++ cinewild/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('movies/', include('movies.urls')),
+    path('', include('movies.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
